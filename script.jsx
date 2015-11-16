a0fe2cd5fadb360a99d4d30ea5a16b8497d58351
Juan C Irizar <juan@up3.com>
2015-11-16 17:01:06 -0500

    fist commit

---
+++ script.jsx
@@ -0,0 +1,23 @@
+var Card = React.createClass({
+  render: function() {
+    return (
+        <div>
+        </div>
+    )
+  }
+});
+
+var Main = React.createClass({
+  render: function() {
+    return (
+        <div>
+          ...
+        </div>
+    );
+  }
+});
+
+
+
+
+ReactDOM.render( <Main /> , document.getElementById('root'));
